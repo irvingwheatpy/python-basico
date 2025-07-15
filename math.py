aa5628495a2544e059e84f69479ec878e4728cf2
irvingwheatpy <irvingwheatjs@gmail.com>
2025-07-15 13:39:51 -0700

    Add files via upload

---
+++ math.py
@@ -0,0 +1,41 @@
+print('This is a sequence of progressivly harder math problems')
+
+print('Q: What is 1+1?')
+
+a = 1
+
+print('A: 1 + 1 =', a + a)
+
+print('Q: What is 2 - 3?')
+
+c = 2
+d = 3
+
+print('A: 2 - 3 =', c - d)
+
+print('Q: What is 2 * 8?')
+
+e = 8
+
+print('A: 2 * 8 =', c * e)
+
+print('Q: John has 18 cans of soda. If he were to split it with himself and 2 others, how much would he have left?')
+
+f = 18
+
+print('A: 18 / 3 =', f / d)
+
+print('Q: If a lumberjack can chop 7 trees in 5.5 days, how much could he chop in 22 days?')
+
+g = 7
+h = 5.5
+i = 22
+
+print('A: (7 / 5.5) * 22 =', (g / h) * 22)
+
+print('Q: What is 13^2 - 100?')
+
+j = 13
+k = 100
+
+print('A: (13 * 13) - 100 =', (j * j) - 100)

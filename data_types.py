7079f7e30cd0455b18a30f79a62b6892ce0d09ae
irvingwheatpy <irvingwheatjs@gmail.com>
2025-08-30 11:04:34 -0700

    Add files via upload

---
+++ data_types.py
@@ -0,0 +1,93 @@
+#Setting the Data Type
+
+#str
+x = "Hello World"
+
+#int
+x = 20
+
+#float
+x = 20.5
+
+#complex
+x = 1j
+
+#list
+x = ["apple", "banana", "cherry"]
+
+#tuple
+x = ("apple", "banana", "cherry")
+
+#range
+x = range(6)
+
+#dict
+x = {"name" : "John", "age" : 36}
+
+#set
+x = {"apple", "banana", "cherry"}
+
+#frozenset
+x = frozenset({"apple", "banana", "cherry"})
+
+#bool
+x = True
+
+#bytes
+x = b"Hello"
+
+#bytearray
+x = bytearray(5)
+
+#memoryview
+x = memoryview(bytes(5))
+
+#NoneType
+x = None
+
+#Setting the Specific Data Type
+
+#str
+x = str("Hello World")
+
+#int
+x = int(20)
+
+#float
+x = float(20.5)
+
+#complex
+x = complex(1j)
+
+#list
+x = list(("apple", "banana", "cherry"))
+
+#tuple
+x = tuple(("apple", "banana", "cherry"))
+
+#range
+x = range(6)
+
+#dict
+x = dict(name = "John", age = 36)
+
+#set
+x = set(("apple", "banana", "cherry"))
+
+#frozenset
+x = frozenset(("apple", "banana", "cherry"))
+
+#bool
+x = bool(5)
+
+#bytes
+x = bytes(5)
+
+#bytearray
+x = bytearray(5)
+
+#memoryview
+x = memoryview(bytes(5))
+
+#NoneType
+x = None
